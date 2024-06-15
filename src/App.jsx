39cd27955e997b6ae35dfaf072be09ad1f44aa91
ArrKuu77 <aungkungsan@gmail.com>
2024-06-15 00:58:32 -1100

    overflow-hidden

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ const App = () => {
   });
 
   return (
-    <div className=" relative  ">
+    <div className=" relative max-sm:overflow-hidden ">
       <div className=" absolute max-sm:relative max-sm:bg-[#B8A47F]/60 w-full top-[-40px] max-sm:top-[-30px] z-30">
         <NavBar />
       </div>
